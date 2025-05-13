bf67d9bed026eb88b754b735ab25ebdcf2c4fab9
Lại Thanh Xuân <125149598+Xuanlai583@users.noreply.github.com>
2025-05-13 22:23:13 +0700

    update login page

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,40 +1,46 @@
+"use client";
+
 import { on } from "events";
-import { Formik } from "formik";
+import { Formik, Form } from "formik";
 import Image from "next/image";
 
 interface formikInitialValues {
-    email: string;
-    password: string;
-}   
+  email: string;
+  password: string;
+}
 
 const initialValues: formikInitialValues = {
   email: "",
   password: "",
 };
 
-
-
+const PrimaryInput = () => {
+  return <input className="w-full" />;
+};
 
 export default function Login() {
-    const onSubmit = (values: formikInitialValues) => {}
-
-
-
+  const onSubmit = (values: formikInitialValues) => {};
 
   return (
-    <main className="pt-24 flex justify-center">
-      <section>
+    <main className="w-full h-full pt-24">
+      <section className="w-full flex flex-col items-center">
         <Image
           width={198}
           height={78}
           src={"/facebook.png"}
           alt={"Facebook icon"}
         />
-      </section>
 
-      <Formik<formikInitialValues> initialValues={initialValues} onSubmit={onSubmit}>
-       
-      </Formik>
+        <Formik<formikInitialValues>
+          initialValues={initialValues}
+          onSubmit={onSubmit}
+        >
+          <Form className="w-full flex flex-col bg-white drop-shadow-sm pt-4 pb-6 max-w-sm px-2">
+            <PrimaryInput/>
+            <PrimaryInput/>
+          </Form>
+        </Formik>
+      </section>
     </main>
   );
 }
